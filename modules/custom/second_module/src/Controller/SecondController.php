73ced85ebb43a4dcccd7e1a33cd67392378614df
MikkeBee <mjblakley87@gmail.com>
2022-08-25 13:57:17 +0300

    added two practice custom modules

---
+++ modules/custom/second_module/src/Controller/SecondController.php
@@ -0,0 +1,10 @@
+<?php 
+
+namespace Drupal\second_module\Controller;
+
+class SecondController {
+
+    public function second() {
+        return array('#markup' => 'Wee, look what I can do!');
+    }
+}
